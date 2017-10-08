e2e10af2bc93ca81629a15b4e4d02598fce295ad
Navjot Singh <weavebytes@gmail.com>
2017-10-08 13:05:20 +0530

    added basic flask and update gitignor

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, render_template
+from eve import Eve
+
+
+# simple flask
+app = Flask(__name__)
+
+
+# use when we use EVE Rest api framework
+# templ_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+# app = Eve('App', template_folder=templ_dir)
+
+
+@app.route('/')
+def home():
+    return 'Welcome to Flask Angular Demo'
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True, threaded=True)
