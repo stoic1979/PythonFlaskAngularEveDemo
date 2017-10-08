59addf78fcfd773358be44561148ef394f64feb8
Navjot Singh <weavebytes@gmail.com>
2017-10-08 16:49:18 +0530

    eve api using angular flask

--- app.py
+++ app.py
@@ -1,19 +1,38 @@
 import os
-from flask import Flask, render_template
+import jsonify
+import json
+import traceback
+from flask import Flask, render_template, request
+from db import Mdb
+from bson import ObjectId
 from eve import Eve
 
 
 # simple flask
-app = Flask(__name__)
+#app = Flask(__name__)
+#mdb = Mdb()
 
 
 # use when we use EVE Rest api framework
-# templ_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-# app = Eve('App', template_folder=templ_dir)
+templ_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Eve('App', template_folder=templ_dir)
+mdb = Mdb()
 
+
+######################################################
+#                                                    #
+# Note: _id of mongodb collection was not getting    #
+# json encoded, so had to create this json encoder   #
+#                                                    #
+######################################################
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
 
 @app.route('/')
-def home():
+def main_page():
     return 'Welcome to Flask Angular Demo'
 
 
@@ -21,6 +40,36 @@ def home():
 def lst():
     templ_data = {'title': 'list_data'}
     return render_template('list.html', **templ_data)
+
+
+@app.route("/add_machine", methods=['POST'])
+def add_machine():
+    try:
+        json_data = request.json['info']
+
+        print "json data:", json_data
+        deviceName = json_data['device']
+        ipAddress = json_data['ip']
+        userName = json_data['username']
+        password = json_data['password']
+        portNumber = json_data['port']
+
+        mdb.save_machine(deviceName, ipAddress, userName, password, portNumber)
+        return jsonify(status='OK', message='inserted successfully')
+
+    except Exception as exp:
+        return jsonify(status='ERROR', message=str(exp))
+
+
+@app.route("/get_machine", methods=['POST'])
+def get_machine():
+
+    try:
+        result = mdb.get_machine_data()
+        return json.dumps(result)
+    except Exception as exp:
+        print 'get_machine() :: Got exception %s' % exp
+        print(traceback.format_exc())
 
 
 if __name__ == '__main__':
---
+++ settings.py
@@ -0,0 +1,65 @@
+import os
+
+
+# mongo online db(MLAB) credentials
+# MONGO_HOST = os.environ.get('MONGO_HOST', '')
+# MONGO_PORT = os. environ.get('MONGO_PORT', '')
+# MONGO_USERNAME = os.environ.get('MONGO_USERNAME', '')
+# MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD', '')
+# MONGO_DBNAME = os.environ.get('MONGO_DBNAME', '')
+
+
+# mongo local db credentials
+MONGO_HOST = os.environ.get('MONGO_HOST', 'localhost')
+MONGO_PORT = os. environ.get('MONGO_PORT', 27017)
+MONGO_USERNAME = os.environ.get('MONGO_USERNAME', 'admin')
+MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD', '123')
+MONGO_DBNAME = os.environ.get('MONGO_DBNAME', 'flask_angular')
+MONGO_AUTH_SOURCE = os.environ.get('MONGO_AUTH_SOURCE', 'admin')
+
+
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+
+ITEM_METHODS = ['GET', 'PATCH', 'DELETE']
+
+CACHE_CONTROL = 'max-age=20'
+CACHE_EXPIRES = 20
+
+URL_PREFIX = "api"
+
+machine = {
+    'item_title': 'machine',
+
+    'schema': {
+        'username': {
+            'type': 'string'
+        },
+
+        'ip': {
+            'type': 'string'
+        },
+
+        'device': {
+            'type': 'string'
+        },
+
+        'password': {
+            'type': 'string'
+        },
+
+        'port': {
+            'type': 'string'
+        }
+    }
+
+}
+
+
+DOMAIN = {
+    'machine': machine
+}
+
+
+if __name__ == '__main__':
+    print('Current settings are')
+    print ('MONGO_HOST: %s' % MONGO_HOST)
